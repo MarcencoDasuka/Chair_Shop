52914054ad5fc073944bfa6d587d0fd01d75d45b
иля <ilia.marcenco@isa.utm.md>
2025-10-02 14:03:28 +0300

    start pet prjct

---
+++ src/test/java/com/ChairShop/ChairShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChairShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChairShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
